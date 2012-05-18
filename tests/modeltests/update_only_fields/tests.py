7b81967729e9c21e9e297cbbe7d0226fe9281901
Anssi Kääriäinen <akaariai@gmail.com>
2012-05-18 15:10:41 +0300

    Added more tests for deferred model .save()
    Added tests for deferred model foreign key and inheritance cases.

--- tests/modeltests/update_only_fields/tests.py
+++ tests/modeltests/update_only_fields/tests.py
@@ -78,6 +78,32 @@ class UpdateOnlyFieldsTests(TestCase):
         s1 = Person.objects.only('name').get(pk=s.pk)
         with self.assertNumQueries(1):
             s1.save()
+    
+    def test_update_fields_inheritance_defer(self):
+        profile_boss = Profile.objects.create(name='Boss', salary=3000)
+        e1 = Employee.objects.create(name='Sara', gender='F',
+            employee_num=1, profile=profile_boss)
+        e1 = Employee.objects.only('name').get(pk=e1.pk)
+        e1.name = 'Linda'
+        with self.assertNumQueries(1):
+            e1.save()
+        self.assertEqual(Employee.objects.get(pk=e1.pk).name,
+                         'Linda')
+
+    def test_update_fields_fk_defer(self):
+        profile_boss = Profile.objects.create(name='Boss', salary=3000)
+        profile_receptionist = Profile.objects.create(name='Receptionist', salary=1000)
+        e1 = Employee.objects.create(name='Sara', gender='F',
+            employee_num=1, profile=profile_boss)
+        e1 = Employee.objects.only('profile').get(pk=e1.pk)
+        e1.profile = profile_receptionist
+        with self.assertNumQueries(1):
+            e1.save()
+        self.assertEqual(Employee.objects.get(pk=e1.pk).profile, profile_receptionist)
+        e1.profile_id = profile_boss.pk
+        with self.assertNumQueries(1):
+            e1.save()
+        self.assertEqual(Employee.objects.get(pk=e1.pk).profile, profile_boss)
 
     def test_update_fields_m2m(self):
         profile_boss = Profile.objects.create(name='Boss', salary=3000)
